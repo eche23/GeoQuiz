d6c691a1f7d457afa1fb7e2c17b82d0a2ce593e6
Echedey Garcias <eche2312@gmail.com>
2018-02-04 14:10:10 +0000

    Initial commit

---
+++ app/src/main/java/eche2312/gmail/com/geoquiz/QuizActivity.java
@@ -0,0 +1,44 @@
+package eche2312.gmail.com.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class QuizActivity extends Activity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mFalseButton = (Button)findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu){
+       getMenuInflater().inflate(R.menu.activity_quiz, menu);
+       return true;
+    }*/
+}
