d63198730182fd52a47927fbfa3e117e3fb77d7c
eched <eche2312@gmail.com>
2018-02-13 22:04:00 +0000

    Metodos ciclo de vida añadidos

--- app/src/main/java/eche2312/gmail/com/geoquiz/QuizActivity.java
+++ app/src/main/java/eche2312/gmail/com/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package eche2312.gmail.com.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -12,10 +13,14 @@ public class QuizActivity extends Activity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    protected final String TAG = this.getClass().getSimpleName();
+    private static final String KEY_INDEX = "index";
+    private int mCurrentIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button)findViewById(R.id.true_button);
@@ -34,11 +39,56 @@ public class QuizActivity extends Activity {
                 Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_LONG).show();
             }
         });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
        getMenuInflater().inflate(R.menu.activity_quiz, menu);
        return true;
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+
     }
 }
